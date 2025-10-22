65bf9adaf88a1892f06b893ebb81a6518f0bc5b1
ismai21k <ismailkabir170@gmail.com>
2025-10-22 21:51:42 +0100

    allow all origins

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin:"*"})); // Enable CORS for all origins
 // app.use(cors({ origin:'https://blog-app-mu-cyan.vercel.app'}));
 
 app.use(express.json());
