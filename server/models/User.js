f60ccb12477966fcc1bfc2fd999941c0dd4aa187
ismai21k <ismailkabir170@gmail.com>
2025-10-22 21:00:58 +0100

    add comment

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true, trim: true},
     email: { type: String, required: true, unique: true},
     password: { type: String, required: true},
-    role: { type: String, enum: ['user', 'admin'], default: 'user' }
+    role: { type: String, enum: ['user', 'admin'], default: 'user' } // Role field added
 });
 
 module.exports = mongoose.model('User', userSchema, );
