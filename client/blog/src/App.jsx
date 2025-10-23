3fbf64307e5782846c1094313002b52237080b92
ismai21k <ismailkabir170@gmail.com>
2025-10-23 21:37:18 +0100

    remove useState

--- client/blog/src/App.jsx
+++ client/blog/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+
 import { Routes, Route } from 'react-router-dom'
 import Register  from './pages/Register.jsx'
 import Login from './pages/Login.jsx'
