5731b4e12eeeea91ed8c6265e58d28f8c0787343
ismai21k <ismailkabir170@gmail.com>
2025-10-23 10:46:16 +0100

    add comment

--- server/models/category.js
+++ server/models/category.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-// category Schema
+// category Schema definition 
 const CategorySchema = new mongoose.Schema({
     name: {type: String, required: true, unique: true, trim: true},
     description: { type: String, trim: true }
